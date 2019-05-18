0e29984dbcfea7b5be686c9ded10e9efa970950b
matthew-halligan <mjhallig@uvm.edu>
2019-05-17 20:44:32 -0400

    added article.php This is generic article page

---
+++ article.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rootmaster
+ * Date: 23/11/18
+ * Time: 08:19 PM
+ */
+
+?>
+<?php include 'header-v2.php'; ?>
+<div class="article-container">
+    <article class="article-article">
+        <div class="main-article-container">
+            <div class="article-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, nesciunt.</div>
+            <div class="article-author">Lorem ipsum.</div>
+            <div class="article-date">Lorem ipsum dolor.</div>
+            <div class="article-picture">
+                <img src="assets/img/break-business-businessman-1571148.jpg" alt="" style="width: 100%; height: auto; margin: 1em 0">
+                <div class="article-body">
+                    <p class="line-height-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, excepturi, iure! Aliquam aperiam aspernatur autem commodi consectetur dicta, ex exercitationem ipsa laudantium libero modi necessitatibus obcaecati officiis optio perferendis porro quaerat quasi quia, repellendus sapiente sequi similique tempora temporibus veritatis vero voluptatem voluptates voluptatum. Cum delectus dolore ducimus error eveniet expedita harum impedit in inventore ipsa labore laboriosam minus neque nisi, numquam porro praesentium provident quae quis ratione similique sit ullam. Asperiores ea labore mollitia quam. A aliquid aperiam assumenda beatae, blanditiis cumque dicta dolor dolorem ducimus fugit in incidunt ipsa magni maiores modi nam quaerat quod ratione tempora temporibus?</p>
+                    <p class="line-height-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti doloribus earum
+                        maiores non ratione saepe suscipit? Autem, corporis cumque doloribus ducimus esse illo itaque
+                        laudantium, nemo odit perferendis praesentium reiciendis vero? Assumenda corporis deleniti excepturi
+                        id necessitatibus nostrum quisquam. Ab iusto, quas? Blanditiis cum, explicabo fuga impedit porro
+                        suscipit.</p>
+                    <p class="line-height-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti doloribus earum
+                        maiores non ratione saepe suscipit? Autem, corporis cumque doloribus ducimus esse illo itaque
+                        laudantium, nemo odit perferendis praesentium reiciendis vero? Assumenda corporis deleniti excepturi
+                        id necessitatibus nostrum quisquam. Ab iusto, quas? Blanditiis cum, explicabo fuga impedit porro
+                        suscipit.</p>
+                    <p class="line-height-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti doloribus earum
+                        maiores non ratione saepe suscipit? Autem, corporis cumque doloribus ducimus esse illo itaque
+                        laudantium, nemo odit perferendis praesentium reiciendis vero? Assumenda corporis deleniti excepturi
+                        id necessitatibus nostrum quisquam. Ab iusto, quas? Blanditiis cum, explicabo fuga impedit porro
+                        suscipit.</p>
+                    <p class="line-height-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deleniti doloribus earum
+                        maiores non ratione saepe suscipit? Autem, corporis cumque doloribus ducimus esse illo itaque
+                        laudantium, nemo odit perferendis praesentium reiciendis vero? Assumenda corporis deleniti excepturi
+                        id necessitatibus nostrum quisquam. Ab iusto, quas? Blanditiis cum, explicabo fuga impedit porro
+                        suscipit.</p>
+                </div>
+            </div>
+            <div class="article-written-by">- Written by: <span class="font-style-italic">Matt Halligan</span> -</div>
+        </div>
+    </article>
+    <aside class="article-aside">
+        <div class="main-aside-container">
+            <div class="article-aside-image">
+                <img src="assets/img/break-business-businessman-1571148.jpg" alt="" style="width: 100%; height: auto;">
+            </div>
+            <div class="article-aside-title"><p class="font-bold">This is the article aside title</p></div>
+            <div class="article-summary" >
+                <p class="line-height-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus at illum maxime officiis
+                    pariatur quidem reiciendis repudiandae rerum vel voluptatem?</p>
+            </div>
+
+        </div>
+        <div class="main-aside-container">
+            <div class="article-aside-image">
+                <img src="assets/img/break-business-businessman-1571148.jpg" alt="" style="width: 100%; height: auto;">
+            </div>
+            <div class="article-aside-title"><p class="font-bold">This is the article aside title</p></div>
+            <div class="article-summary" >
+                <p class="line-height-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus at illum maxime officiis
+                    pariatur quidem reiciendis repudiandae rerum vel voluptatem?</p>
+            </div>
+        </div>
+        <div class="main-aside-container">
+            <div class="article-aside-image">
+                <img src="assets/img/break-business-businessman-1571148.jpg" alt="" style="width: 100%; height: auto;">
+            </div>
+            <div class="article-aside-title"><p class="font-bold">This is the article aside title</p></div>
+            <div class="article-summary" >
+                <p class="line-height-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus at illum maxime officiis
+                    pariatur quidem reiciendis repudiandae rerum vel voluptatem?</p>
+            </div>
+        </div>
+    </aside>
+</div>
+<?php include 'footer.php'; ?>
