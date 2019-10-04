eb8a4c87abb45bd9319014e2d566b412b8a43f85
matthew-halligan <mjhallig@uvm.edu>
2019-10-03 22:57:07 -0400

    changed why us to solutions

--- index.php
+++ index.php
@@ -26,166 +26,167 @@ include "top.php";
     </header>
 
 
-<!-- WHAT WE DO STARTS HERE -->
+    <!-- WHAT WE DO STARTS HERE -->
 
-  <section class="mb-0" id="what-we-do">
-    <h6 class="hide" >Hello</h6>
-    <article class="container">
-      <h2 class="text-center">Why Us?</h2>
-      <article class="row our-offerings">
+    <section class="mb-0" id="what-we-do">
         <h6 class="hide" >Hello</h6>
-        <article class="col-lg-4 ml-auto">
-          <h2>We're Secure!</h2>
-          <p class="lead responsive-design-p">
-              Your Data Privacy is of the utmost importance to us.
-              We take precautions to secure your data from attackers by using encryption and industry best practices.
-          </p>
-          <a href="contact.php"><h5>Contact Us To Learn More!</h5>
-          <hr></a> 
-        </article>
-        <article class="col-lg-4 mr-auto">
-          <h6 class="hide" >Hello</h6>
-            <img class="what-we-do-image" src="images/hard-disk.png" alt="Image of a hard drive">
-        </article>
-      </article>
-      <!-- I got the image above from the link below, it's copyright free -->
-      <!-- https://pixabay.com/vectors/computer-server-data-center-tower-304815/ -->
+        <article class="container">
+            <h2 class="text-center">We Have Your Solution</h2>
+            <article class="row our-offerings">
+                <h6 class="hide" >Hello</h6>
+                <article class="col-lg-4 ml-auto">
+                    <h2>Establishing Web Presence</h2>
+                    <p class="lead responsive-design-p">
+                        Work with us to learn what it means to establish an Internet presence and the very successful methods that
+                        others are using to give their business an edge over their competition.
+                    </p>
+                    <a href="contact.php"><h5>Contact Us To Learn More!</h5>
+                        <hr></a>
+                </article>
+                <article class="col-lg-4 mr-auto">
+                    <h6 class="hide" >Hello</h6>
+                    <img class="what-we-do-image" src="images/hard-disk.png" alt="Image of a hard drive">
+                </article>
+            </article>
+            <!-- I got the image above from the link below, it's copyright free -->
+            <!-- https://pixabay.com/vectors/computer-server-data-center-tower-304815/ -->
 
-      <article class="row our-offerings">
+            <article class="row our-offerings">
+                <h6 class="hide" >Hello</h6>
+                <article class="col-lg-4 ml-auto">
+                    <h6 class="hide" >Hello</h6>
+                    <img class="what-we-do-image" src="images/server.png" alt="Image of a hard drive">
+                </article>
+                <article class="col-lg-4 mr-auto">
+                    <h2>Search Engine Optimization Education</h2>
+                    <p class="lead responsive-design-p">
+                        Halligan Web Development employs ethical search engine optimization techniques to help your business be
+                        the first to appear in front of your customers eyes.  Did you know that the first link on a google search
+                        gets 7x as many clicks as the second link on the page?  Learn how to make this work for you!
+
+                    </p>
+                    <a href="services.php#"><h5>Watch To Learn More!</h5>
+                        <hr></a>
+                </article>
+            </article>
+            <!--
+                  Got the image from the link below, copyright free
+                  https://pixabay.com/illustrations/server-cloud-design-plane-icon-3297974/ -->
+
+            <article class="row our-offerings">
+                <h6 class="hide" >Hello</h6>
+                <article class="col-lg-4 ml-auto">
+                    <h2>Educating You Along The Way</h2>
+                    <p class="lead responsive-design-p">
+                        Here at Halligan Web Development one of our main functions is to educate you about our services and business
+                        as well as the best options to fit your business.
+                    </p>
+                    <a href="contact.php"><h5>Contact Us To Learn More!</h5>
+                        <hr></a>
+                </article>
+                <article class="col-lg-4 mr-auto">
+                    <h6 class="hide" >Hello</h6>
+                    <img class="what-we-do-image" src="images/atom.png" alt="image of data analytics">
+                </article>
+            </article>
+            <!-- Got the image from the link below, it's copyright free
+            https://pixabay.com/vectors/atom-atomic-model-icon-nuclear-1294810/ -->
+
+
+        </article>
+    </section>
+
+    <!-- ADAPTIVE DESIGN STARTS HERE -->
+
+    <section class="mb-0" id="responsive-design">
         <h6 class="hide" >Hello</h6>
-        <article class="col-lg-4 ml-auto">
-          <h6 class="hide" >Hello</h6>
-            <img class="what-we-do-image" src="images/server.png" alt="Image of a hard drive">
-        </article>
-        <article class="col-lg-4 mr-auto">
-            <h2>Fully Managed Web Hosting</h2>
-          <p class="lead responsive-design-p">
-              Spending hours learning why your website is not working is a great waste of your time.
-              We've likely encountered your issue before and can have it solved quickly all while its business as
-              usual for you.
-          </p>
-          <a href="contact.php"><h5>Contact Us To Learn More!</h5>
-          <hr></a>
-        </article>
-      </article>
-<!--
-      Got the image from the link below, copyright free
-      https://pixabay.com/illustrations/server-cloud-design-plane-icon-3297974/ -->
+        <article class="container">
+            <p class="details_data">HWD Was Started to Simplify business solutions to your web troubles. We focus on establishing our customers presence on the internet to share the benefits its provide to thier Business.
+            </p>
+            <p class="details_data">Whether you are just starting out or looking for your next move . You Should join our mailing list to  get our mail tips send directly to your inbox.</p>
+            <form class="sub">
+                <input type ="email" placeholder="enter your email"><button>Join Now</button>
 
-      <article class="row our-offerings">
-        <h6 class="hide" >Hello</h6>
-        <article class="col-lg-4 ml-auto">
-            <h2>Educating You Along The Way</h2>
-          <p class="lead responsive-design-p">
-              Here at Halligan Web Development one of our main functions is to educate you about our services and business
-              as well as the best options to fit your business.
-          </p>
-          <a href="contact.php"><h5>Contact Us To Learn More!</h5>
-          <hr></a> 
-        </article>
-        <article class="col-lg-4 mr-auto">
-          <h6 class="hide" >Hello</h6>
-            <img class="what-we-do-image" src="images/atom.png" alt="image of data analytics">
-        </article>
-      </article>
-      <!-- Got the image from the link below, it's copyright free
-      https://pixabay.com/vectors/atom-atomic-model-icon-nuclear-1294810/ -->
+            </form>
 
-
-    </article>
-  </section>
-
-<!-- ADAPTIVE DESIGN STARTS HERE -->
-
-  <section class="mb-0" id="responsive-design">
-    <h6 class="hide" >Hello</h6>
-    <article class="container">
-        <p class="details_data">HWD Was Started to Simplify business solutions to your web troubles. We focus on establishing our customers presence on the internet to share the benefits its provide to thier Business.
-		</p>
-		<p class="details_data">Whether you are just starting out or looking for your next move . You Should join our mailing list to  get our mail tips send directly to your inbox.</p>
-		<form class="sub">
-		<input type ="email" placeholder="enter your email"><button>Join Now</button>
-		
-		</form>
-
-  </section>
+    </section>
 
 
 
-<!-- PORTFOLIO STARTS HERE -->
-<section class="mb-0" id="index-portfolio">
+    <!-- PORTFOLIO STARTS HERE -->
+    <section class="mb-0" id="index-portfolio">
 
-<h2 class="text-center">Portfolio</h2>
-<p class="text-center line">Halligen Web Development Delivers high value internet presence establishment services .</h2>
-<p class="text-center lines">These Portfolio items are a few examples of successful internet monetization.</h2>
+        <h2 class="text-center">Portfolio</h2>
+        <p class="text-center line">Halligen Web Development Delivers high value internet presence establishment services .</h2>
+        <p class="text-center lines">These Portfolio items are a few examples of successful internet monetization.</h2>
 
-<section class="row">
-<h6 class="hide" >Hello</h6>
-    <article class="card col-sm-12 col-md-9 col-lg-3 view overlay c1" >
-        <h6 class="hide" >Hello</h6>
-        <article class="card-body">
-           <img class="about-us-card-image-portfolio" src="https://media.licdn" alt="">
-            <!--<h5 class="card-title">The Superior Barbershop</h5>-->
-        </article>
+            <section class="row">
+                <h6 class="hide" >Hello</h6>
+                <article class="card col-sm-12 col-md-9 col-lg-3 view overlay c1" >
+                    <h6 class="hide" >Hello</h6>
+                    <article class="card-body">
+                        <img class="about-us-card-image-portfolio" src="https://media.licdn" alt="">
+                        <!--<h5 class="card-title">The Superior Barbershop</h5>-->
+                    </article>
 
-        <a href="https://thesuperiorbarbershop.com" 
-target="_blank" class="big"><article class="mask  rgba-white-strong" > 
-              <h6 class="text-center">Project Description</h6>
-              <p class="black-text:hover">
-                  The Superior Barbershop was in need of a website to improve their online presence outside of Facebook and Instagram.
-                  To achieve the goal of a professional online presence I created a site that brings all of the info to the forefront for anyone to find with ease.
-              </p>
-              <span>
+                    <a href="https://thesuperiorbarbershop.com"
+                       target="_blank" class="big"><article class="mask  rgba-white-strong" >
+                            <h6 class="text-center">Project Description</h6>
+        <p class="black-text:hover">
+            The Superior Barbershop was in need of a website to improve their online presence outside of Facebook and Instagram.
+            To achieve the goal of a professional online presence I created a site that brings all of the info to the forefront for anyone to find with ease.
+        </p>
+        <span>
                     <a href="https://thesuperiorbarbershop.com" target="_blank" class="fa fa-link"></a>
               </span>
         </article></a>
-    </article>
-  
-    <article class="card col-sm-12 col-md-9 col-lg-3 view overlay c2">
-        <h6 class="hide" >Hello</h6>
-        <article class="card-body">
-            <img class="about-us-card-image-portfolio" src="https://media.licdn" alt="">
-            <!--<h5 class="card-title">MTFU Longevity</h5>-->
         </article>
-        <a href="https://mtfucoach.com/shop" target="_blank" class="big" 
-><article class="mask  rgba-white-strong">
-              <h6 class="text-center">Project Description</h6>
-              <p class="black-text">
-                  Mike Dusa of the MTFU Society came to me with a website in need of a store in order to sell his
-                  newly written book.  After determining goals and needs, we delivered a fully functioning store
-                  with the ability to add and remove products with ease as well as integrated PayPal checkout for easy sales.
-              </p>
-              <span>
+
+        <article class="card col-sm-12 col-md-9 col-lg-3 view overlay c2">
+            <h6 class="hide" >Hello</h6>
+            <article class="card-body">
+                <img class="about-us-card-image-portfolio" src="https://media.licdn" alt="">
+                <!--<h5 class="card-title">MTFU Longevity</h5>-->
+            </article>
+            <a href="https://mtfucoach.com/shop" target="_blank" class="big"
+            ><article class="mask  rgba-white-strong">
+                    <h6 class="text-center">Project Description</h6>
+                    <p class="black-text">
+                        Mike Dusa of the MTFU Society came to me with a website in need of a store in order to sell his
+                        newly written book.  After determining goals and needs, we delivered a fully functioning store
+                        with the ability to add and remove products with ease as well as integrated PayPal checkout for easy sales.
+                    </p>
+                    <span>
                     <a href="https://mtfucoach.com/shop" class="fa fa-link"></a>
               </span>
-        </article></a>
-    </article>
-
-    <article class="card col-sm-12 col-md-9 col-lg-3 view overlay c3" >
-        <h6 class="hide" >Hello</h6>
-        <article class="card-body">
-            <img class="about-us-card-image-portfolio" src="https://media.licdn" alt="">
-            <!--<h5 class="card-title">Sunset Shopping Mall</h5>-->
+                </article></a>
         </article>
-         <a href="https://www.sunsetshoppingmall.com" 
-target="_blank" class="big" ><article class="mask  rgba-white-strong">
-              <h6 class="text-center">Project Description</h6>
-              <p class="black-text">
-                  A website designed to display amenities offered by this fictional mall.
-              </p>
-              <span>
+
+        <article class="card col-sm-12 col-md-9 col-lg-3 view overlay c3" >
+            <h6 class="hide" >Hello</h6>
+            <article class="card-body">
+                <img class="about-us-card-image-portfolio" src="https://media.licdn" alt="">
+                <!--<h5 class="card-title">Sunset Shopping Mall</h5>-->
+            </article>
+            <a href="https://www.sunsetshoppingmall.com"
+               target="_blank" class="big" ><article class="mask  rgba-white-strong">
+                    <h6 class="text-center">Project Description</h6>
+                    <p class="black-text">
+                        A website designed to display amenities offered by this fictional mall.
+                    </p>
+                    <span>
                   <a href="https://www.sunsetshoppingmall.com" class="fa fa-link"></a>
               </span>
-        </article></a>
-    </article>
-</section>
-  <section id="portfolio-btn" class="text-center">
-    <h6 class="hide" >Hello</h6>
-	 <p class="text-center text-bottom">Learn more about what we deliver here</p>
-    <a class="btn" href="portfolio.php"> SEE MORE </a>
-  </section>
- 
-</section>
+                </article></a>
+        </article>
+    </section>
+    <section id="portfolio-btn" class="text-center">
+        <h6 class="hide" >Hello</h6>
+        <p class="text-center text-bottom">Learn more about what we deliver here</p>
+        <a class="btn" href="portfolio.php"> SEE MORE </a>
+    </section>
+
+    </section>
 
     <!-- Blog Area Starts Here -->
 
